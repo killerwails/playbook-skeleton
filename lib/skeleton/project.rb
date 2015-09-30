5fa88533a0b358788bc6ec3f598ef6df05c24ce2
Aaron Pederson <aaron.pederson@gmail.com>
2015-09-29 20:13:56 -0700

    Removed JS style concatenation, bad mojo.

--- lib/skeleton/project.rb
+++ lib/skeleton/project.rb
@@ -4,7 +4,7 @@ module Skeleton
   end
   def self.create_required_system_directories
     $required_systems.each do |required_system|
-      Dir.mkdir "playbook-"+$project_name+"/"+required_system unless Dir.exists? "playbook-"+$project_name+"/"+required_system
+      Dir.mkdir "playbook-#{$project_name}/#{required_system}" unless Dir.exists? "playbook-#{$project_name}/#{required_system}"
     end
   end
 end
