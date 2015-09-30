76c083d9bfc41c052e82f17ab2749bb5ba6ba42f
Aaron Pederson <aaron.pederson@gmail.com>
2015-09-29 19:24:48 -0700

    Built out required systems directory creation.

--- lib/skeleton/project.rb
+++ lib/skeleton/project.rb
@@ -2,4 +2,9 @@ module Skeleton
   def self.create_playbook_directory
     Dir.mkdir "playbook-#{$project_name}" unless Dir.exists? "playbook-#{$project_name}"
   end
+  def self.create_required_system_directories
+    $required_systems.each do |required_system|
+      Dir.mkdir "playbook-"+$project_name+"/"+required_system unless Dir.exists? "playbook-"+$project_name+"/"+required_system
+    end
+  end
 end
