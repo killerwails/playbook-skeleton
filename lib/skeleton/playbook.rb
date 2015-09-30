0e7aeee3fa2d0e1893cea79167766bb935ed7a13
Aaron Pederson <aaron.pederson@gmail.com>
2015-09-29 19:25:23 -0700

    Added required_system level playbook and dependencies file creation.

--- lib/skeleton/playbook.rb
+++ lib/skeleton/playbook.rb
@@ -1,9 +1,20 @@
 require 'erubis'
 
 module Skeleton
-  def self.playbook
+  def self.common_playbook
     input = File.read 'templates/playbook.eruby'
     template_file = Erubis::Eruby.new input
     File.write "playbook-#{$project_name}/playbook.yml", template_file.result
+  end
+  def self.required_system_playbook
+    input = File.read 'templates/playbook.eruby'
+    template_file = Erubis::Eruby.new input
+    required_system = $required_systems
+    required_system.each do |required_system|
+      context = {
+        required_system: required_system
+      }
+      File.write "playbook-#{$project_name}/#{required_system}/playbook.yml", template_file.result
+    end
   end
 end
