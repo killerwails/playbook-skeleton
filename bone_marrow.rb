4e88e98790708cf00b451c8d82b782dd436505de
Aaron Pederson <aaron.pederson@gmail.com>
2015-09-29 19:26:18 -0700

    Dropped explicit variable in favour of slimmer template, added unambiguous project name, and re-ordered call list.

--- bone_marrow.rb
+++ bone_marrow.rb
@@ -4,14 +4,16 @@ require 'erubis'
 
 require_relative 'lib/skeleton'
 
-$system_roles = %w(application database management)
-$ansible_roles = %w(nginx php mongodb autodeploy)
 $required_systems = %w(inbound application management database)
-$project_name = 'test'
+$project_name = 'skeleton_commerce'
 $environment = %w(next development staging production)
 
 Skeleton.create_playbook_directory
-Skeleton.playbook
+Skeleton.hosts
+Skeleton.create_required_system_directories
+Skeleton.common_playbook
+Skeleton.required_system_playbook
 Skeleton.infrastructure
 Skeleton.dependencies
-Skeleton.hosts
+Skeleton.required_system_dependencies
+Skeleton.vault
